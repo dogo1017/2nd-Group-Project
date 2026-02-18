e19c6f4c7d693c33d8963316b038aad813c8445e
TheCoolerRyan <ryan.crop@ucas-edu.net>
2026-02-18 09:50:13 -0700

    Finished pseducode

---
+++ pseducode/ryan's_pseducode.py
@@ -0,0 +1,43 @@
+#RC 1st, my pseducode
+
+#Import hashlib
+
+#Create function for logging in
+    #Put the csv into a list of dictionarys
+
+    #Put into loop
+        #Ask them what they want to sign into, or if they want to quit
+        #Get correct input
+    #Put into a loop
+        #if they entered a username, ask them for there password or quit
+            #Get correct input
+        #Else quit back to main
+    #Return list of dictionarys
+
+
+#Function for admin loggin
+    #put into loop
+        #Ask them for username or if they want to quit
+        #If user is adinm than continue
+        #If  username is quit, then break
+        #Else tell them that they have the wrong name
+
+    #put into loop
+        #Ask for password, or quit
+        #If hashed password is adinm password than continue
+        #If  password is quit, then break
+        #Else tell them that they have the wrong password
+
+    #Return password
+
+#Create main function for connecting the admin and user stuff.
+    #Would you like to sign in as a user or admin or quit?
+    #Get propper input
+
+    #Check to see if they signed in as user, if they did make ssure that there is stuff for user sing in
+        #Run loggin function
+        #Esle break
+
+    #If its admin then run admin function
+
+    #If it is quit then return list of dictionarys and quit
