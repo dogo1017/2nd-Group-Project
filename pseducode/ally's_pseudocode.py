1aee89eb962c27e91902a51fb3b6c96806dc10a8
AllyB23 <marlin.rosales2ucas-edu.net>
2026-02-18 09:44:48 -0700

    pseudocode

--- pseducode/ally's_pseudocode.py
+++ pseducode/ally's_pseudocode.py
@@ -1,1 +1,16 @@
-# MR 1st 
+# MR 1st High Score Tracker
+
+
+
+# MAIN FUNCTION
+    #Welcome the user and explain the program
+    #DISPLAY OPTIONS
+    # while True loop
+        # IF statements so the user can choose what to do
+
+# HIGH SCORE TRACKER
+    # Welome them to the high score tracker
+    # Introduce them to the functionality of the program
+
+
+
