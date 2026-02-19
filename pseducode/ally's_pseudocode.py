48fed3690a423b4315c6574bdba6897896b4f644
AllyB23 <marlin.rosales2ucas-edu.net>
2026-02-19 09:32:36 -0700

    high score CSV

--- pseducode/ally's_pseudocode.py
+++ pseducode/ally's_pseudocode.py
@@ -1,16 +1,21 @@
 # MR 1st High Score Tracker
+ # IMPORT CSV
+ #IMPORT OS
 
+# Variable named filename for the CSV
 
+# 
+
+# HIGH SCORE TRACKER
+    # Welome them to the high score tracker
+    # Introduce them to how the program works
+    # make a dictionary for the High score
 
 # MAIN FUNCTION
+    # While True
     #Welcome the user and explain the program
     #DISPLAY OPTIONS
     # while True loop
         # IF statements so the user can choose what to do
-
-# HIGH SCORE TRACKER
-    # Welome them to the high score tracker
-    # Introduce them to the functionality of the program
-
 
 
