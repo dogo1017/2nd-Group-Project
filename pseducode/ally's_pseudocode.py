87925e3d02cec50b7b7a650acf0faf7e4b73c2d1
AllyB23 <marlin.rosales2ucas-edu.net>
2026-02-19 09:50:50 -0700

    pseudocode

--- pseducode/ally's_pseudocode.py
+++ pseducode/ally's_pseudocode.py
@@ -1,21 +1,51 @@
 # MR 1st High Score Tracker
  # IMPORT CSV
- #IMPORT OS
+ # IMPORT OS
 
 # Variable named filename for the CSV
 
-# 
+# Players name is the key and the score is the value
+
+# UPDATE LEADER BOARD FUNCTION
 
 # HIGH SCORE TRACKER
     # Welome them to the high score tracker
     # Introduce them to how the program works
     # make a dictionary for the High score
 
+    # IF statements
+
+        # If user wants to see the top scores
+            # display_top_score()
+            # call the display the top score function
+
+        # Elif user wants to s
+        # LOAD SCORES FUNCTION
+            # 
+
+        # SAVE SCORES FUNCTION
+            # 
+
+        # DISPLAY TOP SCORES
+            # 
 # MAIN FUNCTION
     # While True
     #Welcome the user and explain the program
     #DISPLAY OPTIONS
     # while True loop
         # IF statements so the user can choose what to do
+
+        # IF user wants to play game call the game function
+
+        # ELIF user wants go to the high score tracker call the high score function
+            # high_score_tracker()
+
+        # ELIF user wants to exit the program break the loop
+            # break the WHILE TRUE loop
+            # break
+
+        # ELSE
+            # If the user inputs something else that is not valid, tell them that it is not valid and take them back to the main
+            # "Please enter a valid input."
 
 
