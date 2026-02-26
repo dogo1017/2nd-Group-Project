1e8363d7e743c975da93e5d9d1fb60cd7d07a1a0
AllyB23 <marlin.rosales2ucas-edu.net>
2026-02-26 09:21:56 -0700

    high score tracker page created

---
+++ src/high_score_tracker.py
@@ -0,0 +1,34 @@
+# MR 1st High Score Tracker
+import csv
+
+high_score = ()
+
+def high_score_tracker():
+    pass
+    def display_high_score():
+        pass
+        def save_scores():
+            def load_scores():
+
+    pass
+
+def update_leaderboard():
+    pass
+
+dinosaur_game = ""
+
+def main():
+    while True:
+        print("Welcome to the HIGH SCORE TRACKER!")
+        print("Here you can play the dinosaur game and compare your high score to other users!")
+        choice = input("Would you like to play, view your high score, or view leaderboard")
+        if choice == '1':
+            dinosaur_game()
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        else:
+            break
+
+main()
