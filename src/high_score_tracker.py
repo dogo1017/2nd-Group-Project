7b497c56414ce8721882200f72a5815aefae29ae
AllyB23 <marlin.rosales2ucas-edu.net>
2026-03-05 09:49:58 -0700

    leaderboard

--- src/high_score_tracker.py
+++ src/high_score_tracker.py
@@ -10,6 +10,7 @@ def leader_board(information):
         user.append(i["username"])
 
     top_scores = sorted(scores, reverse = True)[:5]
+    
 
 
 
@@ -17,10 +18,15 @@ def leader_board(information):
         for i in information:
             if int(i["high score"]) == score:
                 top_user.append(i["username"])
+    return top_scores, top_user
 
 
-    print(top_scores)
-    print(top_user)
+
+def display_high_scores(top_scores, top_user):
+    print("--------------------LEADER BOARD--------------------")
+    print("These are the top 5 High Scores !")
+    for x in range(4):
+        print(f"{x+1}: {top_user[x]}, {top_scores[x]}")
 
 def saving_high_score(high_score, users):
     for user in users:
