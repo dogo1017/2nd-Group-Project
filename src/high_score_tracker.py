ada9e7c80db1720befbfad45331bc7dc633a495d
AllyB23 <marlin.rosales2ucas-edu.net>
2026-03-05 09:41:16 -0700

    high score tracker

--- src/high_score_tracker.py
+++ src/high_score_tracker.py
@@ -1,14 +1,30 @@
 # MR 1st High Score Tracker
 
-def display_top_scores(users, limit):
+def leader_board(information):
+    top_scores = []
+    top_user = []
+    scores = []
+    user = []
+    for i in information:
+        scores.append(int(i["high score"]))
+        user.append(i["username"])
+
+    top_scores = sorted(scores, reverse = True)[:5]
 
 
 
+    for score in top_scores:
+        for i in information:
+            if int(i["high score"]) == score:
+                top_user.append(i["username"])
 
 
+    print(top_scores)
+    print(top_user)
 
 def saving_high_score(high_score, users):
     for user in users:
         if user["status"] == "active":
            user["highscore"] = str(high_score)
            return users
+
